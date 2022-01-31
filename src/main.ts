3f62b63112ad78ed8aa917624247792695019305
Carlos Gauto <charlyautomatiza@gmail.com>
2022-01-31 16:37:53 -0300

    feat: multiple-reporters - Cloud collection
    - Add multi-reporters
    - Add Cloud collection example

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import newman from 'newman'; // require newman in your project
 
+// List of reporters
+const reporters = ['junit', 'cli', 'progress', 'json'];
+
 // call newman.run to pass `options` object and wait for callback
 newman.run({
     collection: require('./collections/postman_echo.postman_collection.json'),
-    reporters: 'junit',
-}, function (err) {
+    reporters: reporters,
+}, function (err: Error | null) {
 	if (err) { throw err; }
     console.log('Collection run complete!');
+});
+
+// Cloud Collection example
+newman.run({
+    collection: 'https://www.getpostman.com/collections/f2d90da9138461037858',
+    reporters: reporters,
+}, function (err: Error | null) {
+	if (err) { throw err; }
+    console.log('Cloud Collection run complete!');
 });
